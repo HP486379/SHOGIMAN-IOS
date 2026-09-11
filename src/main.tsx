d1b40926a51af9805f222a7e68cfdb04efc93b12
HP486379 <soffio2@gmail.com>
2026-09-11 20:21:31 +0900

    Load mobile advisor styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/mobile.css';
 import './styles/unit-side.css';
+import './styles/advisor-mobile.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
