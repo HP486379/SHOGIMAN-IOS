6f9447b85cf6cfe2b71cb56c50da0a0f6796d6bc
HP486379 <soffio2@gmail.com>
2026-09-11 20:18:37 +0900

    Load unit side styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/mobile.css';
+import './styles/unit-side.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
